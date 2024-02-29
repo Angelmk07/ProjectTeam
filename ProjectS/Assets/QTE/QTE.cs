31a6f26e080dcef48a47f7f7bc5f73879978200d
Angelmk07 <mkrupin07@gmail.com>
2024-02-29 23:33:28 +0300

    QTE_first level final fix

--- ProjectS/Assets/QTE/QTE.cs
+++ ProjectS/Assets/QTE/QTE.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.EditorCoroutines.Editor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -24,84 +26,99 @@ public class QTE : MonoBehaviour
     private bool QTE_heat = false;
     private bool complit = false;
     private int _StartedTime = 0;
-    public void Update()
+    private Coroutine coroutineQTE;
+    public void Start()
     {
 
-        if (_Started == false)
-        {
-
-            _StartedTime = (int)(Time.realtimeSinceStartup);
-            _Started = true;
-            
-        }
-
-        
-        shouQwe_1.SetActive(true);
-        shouQwe_2.SetActive(true);
-        shouQwe_3.SetActive(true);
-
-            if (Input.GetKeyDown(KeyCode.S ) && TrueQTE_1.color != Color.green)
-                TrueQTE_1.color = Color.green;
-            else if (Input.GetKeyDown(KeyCode.S)&& TrueQTE_1.color == Color.green)
-                TrueQTE_2.color = Color.green;
-            if (Input.GetKey(KeyCode.D)&& TrueQTE_2.color == Color.green)
-            {
-                        TrueQTE_3.color = Color.green;
-                         QTE_heat = true;
-                        complit = true;
-            }
-
-            int timeElapsed = (int)(Time.realtimeSinceStartup) - _StartedTime;
-                if (_time - timeElapsed <= 0||complit)
-                {
-                    _Started = false;
-                    if (QTE_heat)
-                    {
-                        EnemyBar_S.value -= 0.2f;
-                        QTE_heat = false;
-                    }
-                    else
-                    {
-                        healbarcharacter.fillAmount -= 0.2f;
-                    }
-                    complit = false;
-                    shouQwe_1.SetActive(false);
-                    shouQwe_2.SetActive(false);
-                    shouQwe_3.SetActive(false);
-                    TrueQTE_1.color = Color.white;
-                    TrueQTE_2.color = Color.white;
-                    TrueQTE_3.color = Color.white;
-                    StartCoroutine(nameof(StartQTE));
-                }
+        coroutineQTE = StartCoroutine(StartQTE());
             
     }
-
+    void Update()
+    {
+        if (_Started == true)
+        {
+            if (Input.GetKeyDown(KeyCode.S) && TrueQTE_1.color != Color.green)
+                TrueQTE_1.color = Color.green;
+            else if (Input.GetKeyDown(KeyCode.S) && TrueQTE_1.color == Color.green)
+                TrueQTE_2.color = Color.green;
+            if (Input.GetKey(KeyCode.D) && TrueQTE_2.color == Color.green)
+            {
+                TrueQTE_3.color = Color.green;
+                StopCoroutine(coroutineQTE);
+                QTE_Off();
+                EnemyBar_S.value -= 0.2f;
+                _Started = false;
+                coroutineQTE = StartCoroutine(StartQTE());
+                
+            }
+        }
+    }
 
 
 
     IEnumerator StartQTE()
     {
-        yield return new WaitForSeconds(4f);
-
-    }
-    IEnumerator complitsucses()
-    {
-        for (int i = 0; i < 4; i--)
+        while (true)
         {
-            TrueQTE_1.color = Color.white;
-            TrueQTE_2.color = Color.white;
-            TrueQTE_3.color = Color.white;
-            yield return new WaitForSeconds(0.5f);
-            
+            yield return new WaitForSeconds(5f);
+            if (_Started == false)
+            {
+
+                //_StartedTime = (int)(Time.realtimeSinceStartup);
+                _Started = true;
+
+            }
+
+
+            changeVision(true);
+
+
+
+            //int timeElapsed = (int)(Time.realtimeSinceStartup) - _StartedTime;
+            // if (_time - timeElapsed <= 0 || complit)
+            //{
+            yield return new WaitForSeconds(10f);
+            healbarcharacter.fillAmount -= 0.2f;
+
+
+            _Started = false;
+            //    if (QTE_heat)
+            //    {
+            //        EnemyBar_S.value -= 0.2f;
+            //        QTE_heat = false;
+            //    }
+            //    else
+            //    {
+            //        healbarcharacter.fillAmount -= 0.2f;
+            //    }
+            //    complit = false;
+
+            QTE_Off();
+
+
+
+            // }
         }
-        
 
     }
+
+    private void QTE_Off()
+    {
+        changecolor(Color.white);
+        changeVision(false);
+    }
+
     void changecolor(Color color)
     {
-        TrueQTE_1.color = Color.white;
-        TrueQTE_2.color = Color.white;
-        TrueQTE_3.color = Color.white;
+        TrueQTE_1.color = color;
+        TrueQTE_2.color = color;
+        TrueQTE_3.color = color;
+    }
+    void changeVision(bool status)
+    {
+        shouQwe_1.SetActive(status);
+        shouQwe_2.SetActive(status);
+        shouQwe_3.SetActive(status);
     }
 
 }
