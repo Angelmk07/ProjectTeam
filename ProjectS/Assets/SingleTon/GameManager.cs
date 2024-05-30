c5e367288e2500808513a94f652e75c27966d9fe
Angelmk07 <mkrupin07@gmail.com>
2024-05-30 18:00:43 +0300

    GameMod= 0

--- ProjectS/Assets/SingleTon/GameManager.cs
+++ ProjectS/Assets/SingleTon/GameManager.cs
@@ -17,7 +17,7 @@ public class GameManager : MonoBehaviour
         }
     }
     private static GameManager _instance;
-    static public int GameMod = 1;
+    static public int GameMod = 0;
     static public bool IsScenLoad = false;
     private GameManager() { }
 
