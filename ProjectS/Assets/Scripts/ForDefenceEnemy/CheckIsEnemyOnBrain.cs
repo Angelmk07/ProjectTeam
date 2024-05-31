44a5b29fe5a4492f69d889398ac3718ff1b31a06
keeeksik <nanimak7@gmail.com>
2024-05-31 13:45:14 +0300

    g

--- ProjectS/Assets/Scripts/ForDefenceEnemy/CheckIsEnemyOnBrain.cs
+++ ProjectS/Assets/Scripts/ForDefenceEnemy/CheckIsEnemyOnBrain.cs
@@ -5,19 +5,15 @@ using UnityEngine;
 public class CheckIsEnemyOnBrain : MonoBehaviour
 {
     [SerializeField] private GameObject prefOfBite;
-    GameObject canvas;
-    private void Start()
-    {
-        canvas = GameObject.FindGameObjectWithTag("GamePlacesDefence");
-    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "GroundEnemy"|| collision.gameObject.tag =="Flying")
         {
             InteractWhisPLayerBar.ChangeBar(-EnemyPower.EnemyStrength);
             Destroy(collision.gameObject);
-            Instantiate(prefOfBite, gameObject.transform.localPosition,Quaternion.identity,canvas.transform);
-
+            GameObject Fang = Instantiate(prefOfBite, gameObject.transform);
+            Fang.transform.position = Vector3.zero;
         }
     }
 }
